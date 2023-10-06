9bf50271080032fe277be446ea03cbbe6b628ae8
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-06 17:08:16 +0200

    checking if color are reported inside the enum list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 /**
+ * enum for colors
+ */
+const colorList = {
+    ROSSO: 'rosso',
+    ARANCIONE: 'arancione',
+    GIALLO: 'giallo',
+    VERDE: 'verde',
+    BLU: 'blu',
+    VIOLA: 'viola',
+    MARRONE: 'marrone',
+    AZZURRO: 'azzurro',
+    ROSA: 'rosa',
+    BIANCO: 'bianco',
+    GRIGIO: 'grigio',
+    NERO: 'nero'
+}
+
+/**
+ * 
+ * @param {*} color - color given from the function
+ * @returns true if color is in the list, else false
+ */
+function isValidColor(color) {
+    console.log('checking');
+    let refinedColor = color.replace(/\s/g, '').toUpperCase();
+    console.log(colorList.hasOwnProperty(refinedColor));
+    return colorList.hasOwnProperty(refinedColor);
+}
+
+/**
  * function takes all items in the html file with the class @info 
  * stores them in @arrayPass, concat all of their value in @password
  * then, changes .innerHTML of @generatedPassword .
+ * @returns null
  */
 function sendData() {
     let arrayPass = document.getElementsByClassName("info");
@@ -10,15 +41,22 @@ function sendData() {
         password = password + String(arrayPass[i].value);
         //checking all fields compiled
         if (arrayPass[i].value === "") {
+            document.getElementById("errorContainer").innerHTML = "si prega di compilare tutti i campi";
+            document.getElementById("errorContainer").style.display = "block";
+            return;
+        }
+        //checking valid colors
+        if (i === 2 && !isValidColor(arrayPass[2].value)) {
+            document.getElementById("errorContainer").innerHTML = "il colore non risulta nella lista dei colori disponibili";
             document.getElementById("errorContainer").style.display = "block";
             return;
         }
     }
-    //replacing spacing with nothing
-    password = password.replace(/\s/g, '');
-    //printing document only if all fields are compiled    
+    //replacing spacing with nothing, Capital to lowercase
+    password = password.replace(/\s/g, '').toLowerCase();
     document.getElementById("generatedPassword").innerHTML = password + "21";
     document.getElementById("errorContainer").style.display = "none";
     document.getElementById("passContainer").style.display = "block";
     return;
 }
+
