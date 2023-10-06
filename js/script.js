252e6be98da63171d9c71066523ea680c5cefe31
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-06 16:38:11 +0200

    removed eventual user spacing, checking for all fields to be compiled

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-
-
+/**
+ * function takes all items in the html file with the class @info 
+ * stores them in @arrayPass, concat all of their value in @password
+ * then, changes .innerHTML of @generatedPassword .
+ */
 function sendData() {
     let arrayPass = document.getElementsByClassName("info");
     let password = "";
     for (let i = 0; i < arrayPass.length; i++) {
         password = password + String(arrayPass[i].value);
+        //checking all fields compiled
+        if (arrayPass[i].value === "") {
+            document.getElementById("errorContainer").style.display = "block";
+            return;
+        }
     }
+    //replacing spacing with nothing
+    password = password.replace(/\s/g, '');
+    //printing document only if all fields are compiled    
     document.getElementById("generatedPassword").innerHTML = password + "21";
+    document.getElementById("errorContainer").style.display = "none";
+    document.getElementById("passContainer").style.display = "block";
+    return;
 }
